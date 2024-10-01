6f9e2e0fc45c48102b0ca813f96cdbc04431517b
swiftBytee <skmehebubali34@gmail.com>
2024-10-01 18:47:14 +0530

    Api discussed

--- src/index.js
+++ src/index.js
@@ -2,14 +2,31 @@
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 // import dotenv from "dotenv";
+import express from "express";
 import "dotenv/config";
 import connectDB from "./db/index.js";
 
+const app = express();
 // dotenv.config({
 //     path: "./env",
 // });
-
-connectDB();
+const Port = process.env.PORT || 8000;
+connectDB()
+    .then(() => {
+        app.on("error", () => {
+            console.log("ERROR: ", error);
+            throw error;
+        });
+        app.listen(Port, () => {
+            console.log(`Server is running at port ${Port}`);
+        });
+        app.get("/", (req, res) => {
+            res.send("Mai taiyar hoon");
+        });
+    })
+    .catch((error) => {
+        console.log(`MongoDB connection error: ${error}`);
+    });
 
 /*
 import express from "express";
